ae8292dda105d7f517c1076e4e753b32abea0641
Sarah Powazek <powazek@mit.edu>
2017-11-10 15:20:10 -0500

    Adding firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FirebaseProvider } from '../providers/firebase/firebase';
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseProvider } from './../providers/firebase/firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKlQyexhP2cAwsNVHA1jkSb3d1u0VKVNE",
+    authDomain: "fir-c14a3.firebaseapp.com",
+    databaseURL: "https://fir-c14a3.firebaseio.com",
+    projectId: "fir-c14a3",
+    storageBucket: "fir-c14a3.appspot.com",
+    messagingSenderId: "1079295813768"
+  };
 
 @NgModule({
   declarations: [
@@ -14,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +41,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    FirebaseProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
+import { FirebaseProvider } from './../../providers/firebase/firebase';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { FirebaseListObservable } from 'angularfire2/database';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  shoppingItems: FirebaseListObservable<any[]>;
+  newItem = '';
+ 
+  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider) {
+    this.shoppingItems = this.firebaseProvider.getShoppingItems();
   }
-
+ 
+  addItem() {
+    this.firebaseProvider.addItem(this.newItem);
+  }
+ 
+  removeItem(id) {
+    this.firebaseProvider.removeItem(id);
+  }
 }
